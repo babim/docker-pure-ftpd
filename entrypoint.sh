1db5f5cfb84ba6137e430b718d3932468c8e670b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-06 16:08:42 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@ if [ -z "`ls /etc/pure-ftpd`" ]
 then
 	cp -R /etc-start/pure-ftpd/* /etc/pure-ftpd
 fi
+# set password root is root
+SSHPASS1=${SSHPASS:-root}
+echo "root:$SSHPASS1" | chpasswd
 service ssh restart
 /usr/sbin/pure-ftpd -c 50 -C 10 -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -P $PUBLICHOST -p 30000:30009
